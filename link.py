649fff01202fac0763a854944e4a923d73334f1d
Håkon Bårsaune <haakonbaa@gmail.com>
2024-11-07 16:51:41 +0100

    Start implementing model from Robot

--- link.py
+++ link.py
@@ -1,10 +1,13 @@
 import numpy as np
 import sympy as sp
-from numpy.typing import NDArray
+from sympy.matrices import MatrixBase
 from util import is_spd
+from numpy.typing import NDArray
 
 from se3 import SE3
 
+
+"""
 class Position():
     def __init__(self, x: np.float64, y: np.float64, z: np.float64):
         self.x = np.float64(x)
@@ -16,16 +19,23 @@ class Position():
 
     def __repr__(self):
         return f"Position(x={self.x}, y={self.y}, z={self.z})"
+"""
 
 
 class Link():
+    """
+    Represents a rigid body link in a robot.
+    """
+
     def __init__(self,
-                 mass: np.float64,
-                 volume: np.float64,
+                 mass: np.float64 | float,
+                 volume: np.float64 | float,
                  inertia: NDArray[np.float64],
                  added_mass: NDArray[np.float64],
                  linear_damping: NDArray[np.float64],
                  quadratic_damping: NDArray[np.float64],
+                 center_of_mass: NDArray[np.float64] = np.zeros((3,)),
+                 center_of_buoyancy: NDArray[np.float64] = np.zeros((3,))
                  ):
         assert type(inertia) == np.ndarray
         assert inertia.shape == (3, 3)
@@ -49,17 +59,94 @@ class Link():
         self.added_mass = added_mass
         self.linear_damping = linear_damping
         self.quadratic_damping = quadratic_damping
+        self.center_of_mass = center_of_mass
+        self.center_of_buoyancy = center_of_buoyancy
 
     def __repr__(self) -> str:
         return f"Link(mass={self.mass},\n" \
-                    f"volume={self.volume},\n" \
-                    f"inertia={self.inertia})\n" \
-                    f"added_mass={self.added_mass})\n" \
-                    f"linear_damping={self.linear_damping})\n" \
-                    f"quadratic_damping={self.quadratic_damping})"
+            f"volume={self.volume},\n" \
+            f"inertia={self.inertia})\n" \
+            f"added_mass={self.added_mass})\n" \
+            f"linear_damping={self.linear_damping})\n" \
+            f"quadratic_damping={self.quadratic_damping})"
 
 
 class Robot():
-    def __init__(self, links: list[Link], transforms: list[SE3]):
-        self.links = links
-        self.transforms = transforms
+    def __init__(self, links: list[Link],
+                 transforms: list[SE3],
+                 params: list[sp.Symbol],
+                 diff_params: list[sp.Symbol]):
+        """
+        A Robot is a collection of links and transformations describing their
+        relative positions and orientations.
+
+        @param links: A list of Link objects.
+        @param transforms: A list of SE3 objects.
+        @param variables: A list of sympy symbols representing the parameters.
+            This list should contain exactly the same symbols as in all transforms.
+        @param diff_variables: A list of sympy symbols representing the differential parameters.
+            This list should contain the derivative of each symbol in @param variables.
+            and in the same order.
+
+        We denote by q the generalized coordinates (defined by the params list)
+        and dq the time derivative of q (q_dot or \dot{q}) if you will).
+        """
+        assert len(links) == len(transforms)
+        assert len(params) == len(diff_params)
+        for var in params:
+            assert isinstance(var, sp.Symbol)
+        for diff_var in diff_params:
+            assert isinstance(diff_var, sp.Symbol)
+
+        # Make sure all parameters used in the transforms are present in the
+        # params list.
+        transform_symbols = set()
+        param_symbols = set(params)
+        for t in transforms:
+            transform_symbols.update(t.free_symbols())
+
+        if transform_symbols != param_symbols:
+            if transform_symbols.issubset(param_symbols):
+                raise ValueError("Extra parameters in the params list: " \
+                        f"{param_symbols - transform_symbols}. " \
+                        "These parameters were not found in the transforms.")
+            else:
+                raise ValueError("Missing paramaters in the params list: " \
+                        f"{transform_symbols - param_symbols}. " \
+                        "These parameters were not found in the transforms.")
+
+        for diff_param in diff_params:
+            if diff_param in param_symbols:
+                raise ValueError(f"Diff param {diff_param} found in params " \
+                                 "list. These symbols should be unique!")
+
+        self._links = links
+        self._transforms = transforms
+        self._link_count: int = len(links)
+        self._params = params
+        self._diff_params = diff_params
+        self._q = sp.Matrix(params)
+        self._dq = sp.Matrix(diff_params)
+
+    def get_dof(self) -> int:
+        """
+        Returns the number of degrees of freedom of the robot. The number of
+        generalized coordinates typically denoted by q.
+        """
+        return len(self._params)
+
+    def get_jacobian(self) -> MatrixBase:
+        """
+        Returns the Jacobian matrix of the robot.
+
+        The Jacobian matrix, J(q), maps the generalized velocities dq to the linear
+        and angular velocities of each link in the robot.
+            v = J(q) * dq
+        J is a (6 * link_count) x dof matrix
+        """
+
+    def get_link_count(self) -> int:
+        return self._link_count
+
+    def get_model(slef):
+        pass
--- main.py
+++ main.py
@@ -2,40 +2,52 @@ import numpy as np
 from sympy import Symbol
 import sympy as sp
 
-from link import Link, Position, Robot
-#from se3 import SE3, set_simplify
-
+from link import Link, Robot
 from se3 import SE3, set_simplify, rot_x, rot_y, rot_z, trans, inv
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.widgets import Slider
 
+
 def main():
     # NED frame, link1 furthest north
     l1 = Link(1.0, 1.0, np.eye(3), np.eye(6), np.eye(6), np.eye(6))
-    l2 = Link(1.0, 1.0, np.eye(3), np.eye(6), np.eye(6), np.eye(6)) # body
+    l2 = Link(1.0, 1.0, np.eye(3), np.eye(6), np.eye(6), np.eye(6))  # body
     l3 = Link(1.0, 1.0, np.eye(3), np.eye(6), np.eye(6), np.eye(6))
 
-    set_simplify(False)
+    set_simplify(False) # Dont simplify SE3 expressions
 
-    xn, yn, zn = sp.symbols('xn yn zn')
-    phi, theta, psi = sp.symbols('phi theta psi')
-    a1, a2, a3, a4 = sp.symbols('a1 a2 a3 a4')
+    xn, yn, zn = sp.symbols('xn yn zn', real=True)
+    phi, theta, psi = sp.symbols('phi theta psi', real=True)
+    a1, a2, a3, a4 = sp.symbols('a1 a2 a3 a4', real=True)
+
+    dxn, dyn, dzn = sp.symbols('dxn dyn dzn', real=True)
+    dphi, dtheta, dpsi = sp.symbols('dphi dtheta dpsi', real=True)
+    da1, da2, da3, da4 = sp.symbols('da1 da2 da3 da4', real=True)
+
 
     a = SE3()
     Tl2n = trans(xn, yn, zn) @ rot_z(psi) @ rot_y(theta) @ rot_x(phi)
     Tl1l2 = trans(1, 0, 0) @ rot_y(a3) @ rot_z(a4) @ trans(1, 0, 0)
-    Tl3l2 = inv(Tl1l2.copy())
+    Tl3l2 = inv(trans(1, 0, 0) @ rot_y(a1) @ rot_z(a2) @ trans(1, 0, 0))
 
     Tl1n = Tl2n @ Tl1l2
     Tl3n = Tl2n @ Tl3l2
 
-    eely = Robot(links = [l1, l2, l3], transforms = [Tl1n, Tl2n, Tl3n])
+    eely = Robot(links=[l1, l2, l3],
+                 transforms=[Tl1n, Tl2n, Tl3n],
+                 params=[xn, yn, zn, phi, theta, psi, a1, a2, a3, a4],
+                 diff_params=[dxn, dyn, dzn, dphi, dtheta, dpsi, da1, da2, da3, da4]
+            )
 
+    model = eely.get_model()
+
+    print(model)
 
     v = np.array([0, 0, 0])
-    subs = {xn: 0, yn: 0, zn: 0, phi: 0, theta: 0, psi: 0, a1: sp.pi/3, a2: 0, a3: sp.pi/3, a4: 0}
+    subs = {xn: 0, yn: 0, zn: 0, phi: 0, theta: 0,
+            psi: 0, a1: sp.pi/3, a2: 0, a3: sp.pi/3, a4: 0}
     p1 = np.array(Tl1n.apply(v).evalf(subs=subs)).astype(np.float64)
     p2 = np.array(Tl2n.apply(v).evalf(subs=subs)).astype(np.float64)
     p3 = np.array(Tl3n.apply(v).evalf(subs=subs)).astype(np.float64)
@@ -44,17 +56,17 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     fig.subplots_adjust(bottom=0.25)
-    
+
     # Set axis labels to NED
     ax.set_xlabel('east')
     ax.set_ylabel('north')
     ax.set_zlabel('negative down')
-    
+
     # Invert the z-axis to make it point downwards
     # ax.set_zlim(10, 0)  # Set limits to make positive values point downward
-    
+
     # Example data to plot (replace with your actual data)
-    
+
     # Plot data in NED frame
     #ax.plot(p1[0], p1[1], p1[2], marker='o', color='r')
     #ax.plot(p2[0], p2[1], p2[2], marker='o', color='g')
@@ -67,18 +79,19 @@ def main():
     ax.set_zlim([-1, 1])
 
     p1_line, = ax.plot(p1[0], p1[1], p1[2], marker='o', color='r')
-    p1_arrows = ax.quiver(np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3), color='r')
+    p1_arrows = ax.quiver(np.zeros(3), np.zeros(3), np.zeros(
+        3), np.zeros(3), np.zeros(3), np.zeros(3), color='r')
     p2_line, = ax.plot(p2[0], p2[1], p2[2], marker='o', color='g')
     p3_line, = ax.plot(p3[0], p3[1], p3[2], marker='o', color='b')
-    #ax.set_ylim(ax.get_xlim()[::-1])
-    
+    # ax.set_ylim(ax.get_xlim()[::-1])
+
     # Define the slider update function
     def update(val):
         # Update the substitution dictionary with the slider values
         subs[phi] = slider_phi.val
         subs[theta] = slider_theta.val
         subs[psi] = slider_psi.val
-    
+
         vo = np.array([0, 0, 0])
         v1 = np.array([1, 0, 0])
         v2 = np.array([0, 1, 0])
@@ -89,14 +102,13 @@ def main():
         p12 = np.array(Tl1n.apply(v2).evalf(subs=subs)).astype(np.float64)
         p13 = np.array(Tl1n.apply(v3).evalf(subs=subs)).astype(np.float64)
 
-
         p2 = np.array(Tl2n.apply(vo).evalf(subs=subs)).astype(np.float64)
         p3 = np.array(Tl3n.apply(vo).evalf(subs=subs)).astype(np.float64)
 
         p1 = ned_to_xyz @ p1
         p2 = ned_to_xyz @ p2
         p3 = ned_to_xyz @ p3
-    
+
         # Update the plot data
         p1_line.set_data(p1[0], p1[1])
         p1_line.set_3d_properties(p1[2])
@@ -104,34 +116,36 @@ def main():
         p2_line.set_3d_properties(p2[2])
         p3_line.set_data(p3[0], p3[1])
         p3_line.set_3d_properties(p3[2])
-    
+
         # Redraw the figure
         fig.canvas.draw_idle()
-    
+
     # Create sliders for roll (phi), pitch (theta), and yaw (psi)
-    ax_phi = plt.axes([0.1, 0.01, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-    ax_theta = plt.axes([0.1, 0.06, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-    ax_psi = plt.axes([0.1, 0.11, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+    ax_phi = plt.axes([0.1, 0.01, 0.65, 0.03],
+                      facecolor='lightgoldenrodyellow')
+    ax_theta = plt.axes([0.1, 0.06, 0.65, 0.03],
+                        facecolor='lightgoldenrodyellow')
+    ax_psi = plt.axes([0.1, 0.11, 0.65, 0.03],
+                      facecolor='lightgoldenrodyellow')
     #ax_phi = fig.add_axes([0.25, 0.1, 0.65, 0.03])
     #ax_theta = fig.add_axes([0.1, 0.06, 0.65, 0.03], facecolor='lightgoldenrodyellow')
     #ax_psi = fig.add_axes([0.1, 0.11, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-    
+
     slider_phi = Slider(ax_phi, 'Roll (phi)', -np.pi, np.pi, valinit=0)
-    slider_theta = Slider(ax_theta, 'Pitch (theta)', -np.pi/2, np.pi/2, valinit=0)
+    slider_theta = Slider(ax_theta, 'Pitch (theta)', -
+                          np.pi/2, np.pi/2, valinit=0)
     slider_psi = Slider(ax_psi, 'Yaw (psi)', -np.pi, np.pi, valinit=0)
-    
+
     # Register the update function to the sliders
     slider_phi.on_changed(update)
     slider_theta.on_changed(update)
     slider_psi.on_changed(update)
     update(None)
-    
+
     # Show the plot
     plt.show()
-        # Show the plot
-        #plt.show()
-    
-
+    # Show the plot
+    # plt.show()
 
     """
     r = Robot([l1, l2, l3], [])
@@ -144,6 +158,7 @@ def main():
 
     r.to_sdf() # gazebo
     """
+
 
 if __name__ == "__main__":
     main()
