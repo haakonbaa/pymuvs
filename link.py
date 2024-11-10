Refactor and fix jacobian error

link.py
@@ -4,14 +4,19 @@ from sympy.matrices import MatrixBase
 from util import is_spd, is_symmetric
 from numpy.typing import NDArray
 
+from deprecated import deprecated
+
 from se3 import SE3, rotmat_to_angvel_matrix_frameb
+from util import jacobian as _jacobian
+
 
 class Model():
     """
     A mathematical model of a robot on the form
         M(q) * ddq + C(q, dq) dq + D(q, dq) dq + g(q) = tau
     """
-    #TODO: make sure it is C(q, dq) dq and small g(q) everywhere in the code.
+    # TODO: make sure it is C(q, dq) dq and small g(q) everywhere in the code.
+
     def __init__(self, M: MatrixBase, C: MatrixBase, D: MatrixBase, g: MatrixBase):
         assert M.shape[0] == M.shape[1]
         n = M.shape[0]
@@ -147,6 +152,7 @@ class Robot():
         """
         return len(self._links)
 
+    @deprecated
     def get_jacobian(self) -> MatrixBase:
         """
         Returns the Jacobian matrix of the robot.
@@ -165,6 +171,7 @@ class Robot():
         for link_num in range(nl):
             Ji = sp.zeros(6, ndof)
 
+            # This is wrong. The p vector is given by
             p = self._transforms[link_num].get_translation()
             R = self._transforms[link_num].get_rotation()
 
@@ -175,7 +182,7 @@ class Robot():
 
         return J
 
-    def get_model(self, gvec : NDArray[np.float64] = np.array([0,0,-9.81])) -> None:
+    def get_model(self, gvec: NDArray[np.float64] = np.array([0, 0, -9.81])) -> None:
         """
         Returns the robot model as a Model object. The model object represents
         a mathematical model on the form
@@ -193,7 +200,11 @@ class Robot():
             assert gvec.size == 3
             gvec.reshape((3, 1))
 
-        J = self.get_jacobian()
+        # stack the jacobian matrices
+        J = sp.zeros(6 * self.get_link_count(), self.get_dof())
+        for t_num, T in enumerate(self._transforms):
+            mi = 6 * t_num
+            J[mi:mi+6, :] = T.get_jacobian(self._params)
 
         # Mass matrix
         M = sp.zeros(6*self.get_link_count(), 6*self.get_link_count())
@@ -219,7 +230,7 @@ class Robot():
         # Coreolis matrix
         C = sp.zeros(self.get_dof(), self.get_dof())
 
-        C += J.T * M * Jd + Jd.T * M * J # TODO: verify this is correct
+        C += J.T * M * Jd + Jd.T * M * J  # TODO: verify this is correct
 
         ugly = J.T * M * J * self._dq
         C += 0.5 * _jacobian(ugly, self._q).T
@@ -235,7 +246,8 @@ class Robot():
                 # TODO: adjust for center of mass
                 qi = sp.Matrix([self._params[i]])
                 _jacobian(pos, qi)
-                g[i] += _jacobian(pos, sp.Matrix([self._params[i]])).T @ gvec * link.mass
+                g[i] += _jacobian(pos, sp.Matrix([self._params[i]])
+                                  ).T @ gvec * link.mass
 
         g = - sp.simplify(g)
 
@@ -251,8 +263,7 @@ class Robot():
         return Model(Ma, C, sp.zeros(self.get_dof(), self.get_dof()), g)
 
 
-
-def _time_diff_matrix(A : MatrixBase, q: list[sp.Symbol], dq: list[sp.Symbol]) -> MatrixBase:
+def _time_diff_matrix(A: MatrixBase, q: list[sp.Symbol], dq: list[sp.Symbol]) -> MatrixBase:
     """
     Compute the time derivative of a matrix A. A is a function of q, and the
     time derivatives of q are given by dq.
@@ -265,37 +276,3 @@ def _time_diff_matrix(A : MatrixBase, q: list[sp.Symbol], dq: list[sp.Symbol]) -
             Ad[r, c] = A[r, c].diff(sp.Matrix(q)).T @ sp.Matrix(dq)
 
     return Ad
-
-
-def _jacobian(x: MatrixBase, q: MatrixBase) -> MatrixBase:
-    """
-    Compute the Jacobian of a nx1 vector x with respect to a list of
-    parameters q.
-    """
-
-    assert ((x.shape[0] == 1) or (x.shape[1] == 1))
-    assert q.shape[1] == 1
-
-    if (x.shape[0] == 1) and (x.shape[1] != 1):
-        return _jacobian(x.T, q).T
-
-    nx = x.shape[0]
-    nq = q.shape[0]
-
-    J = sp.zeros(nx, nq)
-    for i in range(nx):
-        J[i, :] = x[i].diff(q).T
-
-    return J
-
-
-def main():
-    x, y, z = sp.symbols('x y z')
-    m = sp.Matrix([[x+y**2 + x*z], [x*y*z]])
-    q = sp.Matrix([x, y, z])
-    J = _jacobian(m, q)
-    print(J, J.shape)
-
-
-if __name__ == "__main__":
-    main()

link_test.py
@@ -2,29 +2,12 @@ import unittest
 import sympy as sp
 import numpy as np
 
-from se3 import trans, rot_x
-from link import _jacobian, _time_diff_matrix, Link, Robot
+from util import jacobian
+from se3 import rot_x, trans
+from link import Link, Model, _time_diff_matrix, Robot
+
 
 class TestLink(unittest.TestCase):
-
-    def test_jacobian(self):
-        x, y, z = sp.symbols('x y z')
-
-        q = sp.Matrix([x, y, z])
-
-        m1 = sp.Matrix([[x]])
-        dm1 = _jacobian(m1, q)
-        self.assertEqual(dm1, sp.Matrix([[1, 0, 0]]))
-
-        m2 = sp.Matrix([[x + y**2 + x*z], [x*y*z]])
-        dm2 = _jacobian(m2, q)
-        self.assertEqual(dm2, sp.Matrix([[1 + z, 2*y, x],
-                                        [y*z, x*z, x*y]]))
-
-        m3 = sp.Matrix([[0], [sp.sin(x)], [-sp.cos(x)]])
-        qi = sp.Matrix([x])
-        dm3 = _jacobian(m3, qi)
-
 
     def test_time_diff_matrix(self):
         x, y = sp.symbols('x y')
@@ -42,6 +25,7 @@ class TestLink(unittest.TestCase):
         dC = _time_diff_matrix(C, [x, y], [dx, dy])
         self.assertEqual(dC, sp.Matrix([[dx + dy], [x*dy + y*dx]]))
 
+
 class TestSystem(unittest.TestCase):
 
     def test_pendulum(self):
@@ -55,14 +39,15 @@ class TestSystem(unittest.TestCase):
 
         theta, dtheta = sp.symbols('θ dθ')
         Tbn = rot_x(theta) @ trans(0, 0, -l)
-        mass = Link(m, 0, np.zeros((3,3)), np.zeros((6,6)), np.zeros((6,6)), np.zeros((6,6)))
+        mass = Link(m, 0, np.zeros((3, 3)), np.zeros(
+            (6, 6)), np.zeros((6, 6)), np.zeros((6, 6)))
 
-        pendulum = Robot(links = [mass],
-                         transforms = [Tbn],
-                         params = [theta],
-                         diff_params = [dtheta])
+        pendulum = Robot(links=[mass],
+                         transforms=[Tbn],
+                         params=[theta],
+                         diff_params=[dtheta])
 
-        model = pendulum.get_model(gvec = np.array([0, 0, -g]))
+        model = pendulum.get_model(gvec=np.array([0, 0, -g]))
         self.assertEqual(model.M, sp.Matrix([[1]]))
         self.assertEqual(model.C, sp.Matrix([[0]]))
         self.assertEqual(model.D, sp.Matrix([[0]]))

main.py
@@ -13,9 +13,12 @@ from matplotlib.widgets import Slider
 
 def main():
     # NED frame, link1 furthest north
-    l1 = Link(1.0, 1.0, np.eye(3), np.zeros((6,6)), np.zeros((6,6)), np.zeros((6,6)))
-    l2 = Link(1.0, 1.0, np.eye(3), np.zeros((6,6)), np.zeros((6,6)), np.zeros((6,6)))  # body
-    l3 = Link(1.0, 1.0, np.eye(3), np.zeros((6,6)), np.zeros((6,6)), np.zeros((6,6)))
+    l1 = Link(1.0, 1.0, np.eye(3), np.zeros((6, 6)),
+              np.zeros((6, 6)), np.zeros((6, 6)))
+    l2 = Link(1.0, 1.0, np.eye(3), np.zeros((6, 6)),
+              np.zeros((6, 6)), np.zeros((6, 6)))  # body
+    l3 = Link(1.0, 1.0, np.eye(3), np.zeros((6, 6)),
+              np.zeros((6, 6)), np.zeros((6, 6)))
 
     set_simplify(True)  # Dont simplify SE3 expressions
 
@@ -55,6 +58,10 @@ def main():
 
     print("started model computation")
     model = t.get_model()
+    print(f"{model.M=}")
+    print(f"{model.C=}")
+    print(f"{model.D=}")
+    print(f"{model.g=}")
     exit()
 
     print(model)